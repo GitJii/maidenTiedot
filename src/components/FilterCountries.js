f4dfde7f03812f9927aeca20abb61d2f5bdc2bdf
Jaakko <jaska919@hotmail.com>
2018-09-15 17:50:01 +0300

    Tehtävä_2.13 (kesken)
    Sovellus tekeminen on kesken, sovelluksessa maan nimen klikkaaminen ei toimi tarkoitetulla tavalla.

--- src/components/FilterCountries.js
+++ src/components/FilterCountries.js
@@ -8,15 +8,31 @@ const FilterCountries = ({ tila }) => {
             country.name.includes(tila.filter))
 
     const result = () => {
+
+        const showSpecificCountry = ({country}) => {
+            return () => 
+                <Country key={country.name}
+                    country={country}
+                />
+            }
+
         if (filteredCountries.length > 10) {
             return 'too many matches, you have to be more specific than that'
 
         } else if (filteredCountries.length > 1) {
-            return filteredCountries.map(country => <li key={country.name}> {country.name}</li>)
+            return filteredCountries.map(country =>
+                <div key={country.name}>
+                    <div onClick={() => showSpecificCountry(country)}>
+                        {country.name}
+                    </div>
+                </div>
+            )
 
         } else {
-            return filteredCountries.map(country => <Country key={country.name} country={country} />)
-
+            return filteredCountries.map(country =>
+                <Country key={country.name}
+                    country={country}
+                />)
         }
     }
 
