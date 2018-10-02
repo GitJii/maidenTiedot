ec8ff23c4395f21a21bc2a3a203f49ddd7a1bbf6
Jaakko <jaska919@hotmail.com>
2018-10-02 12:48:21 +0300

    Tehtävä_2.13_(kesken)
    result ei toimi.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import axios from 'axios'
-import FilterCountries from './components/FilterCountries';
+import axios from 'axios';
+import Country from './components/Country'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       countries: [],
-      filter: ''
+      filter: '',
+      specificCountry: undefined
     }
   }
-
-
 
   componentDidMount() {
 
@@ -22,27 +21,63 @@ class App extends React.Component {
       })
   }
 
-
   handleCountryChange = (event) => {
     console.log(event.target.value)
     this.setState({ filter: event.target.value })
   }
 
+  specificCountryPressed = ({ country }) => {
+    this.setState({ specificCountry: country })
+  }
+
+  result = () => {
+    const filteredCountries =
+      this.state.countries.filter(country =>
+        country.name.includes(this.state.filter))
+
+    if (filteredCountries.length > 10) {
+      return 'too many matches, you have to be more specific than that'
+
+    } else if (filteredCountries.length > 1) {
+
+      if (this.state.specificCountry) {
+        return (filteredCountries.map(country =>
+          <div key={country.name}>
+            <div onClick={
+
+              this.specificCountryPressed({ country })
+
+              /* + console.log('nappia painettu', country.name) */
+
+            }>
+              {country.name}
+            </div>
+          </div>)
+        )
+      }
+
+      else {
+        return (filteredCountries.map(country =>
+          <Country key={country.name}
+            country={country}
+          />)
+        )
+      }
+    }
+  }
+
   render() {
 
     return (
-
       <div>
         find countries: <input
           value={this.state.filter}
-          onChange={this.handleCountryChange}
-        />
-
+          onChange={this.handleCountryChange} />
         <div>
-          <FilterCountries tila={this.state}/>
+          {result()}
         </div>
-      </div>
 
+      </div>
     )
   }
 }
